9bd91360af655649b0b1f5b8f3ba1108dbfc4712
JongKwang Kim <kim@jongkwang.com>
2018-08-07 14:54:48 +0900

    sum() 추가

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,10 @@ package com.mycompany.test
 
 public class Calculator {
   public static void main( String[] args) {
-    
+
+  }
+
+  public static int sum(int a, int b) {
+    return a + b;
   }
 }
